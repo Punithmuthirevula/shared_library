b4d8042adf08f21ee98c5c2eab69b3e769d5b99c
Punithmuthirevula <35683475+Punithmuthirevula@users.noreply.github.com>
2025-12-07 22:57:44 +0530

    Update greet.groovy

--- vars/greet.groovy
+++ vars/greet.groovy
@@ -1,3 +1,24 @@
-def call(name, location) {
-   echo "Welcome ${name}, Welcome to ${location}"
+def call(tomcatServer, artifact_id) {
+   // echo "Welcome ${name}, Welcome to ${location}"
+   sshPublisher(
+                        publishers: [
+                            sshPublisherDesc(
+                                configName: '${tomcatServer}   ',
+                                transfers: [
+                                    sshTransfer(
+                                        cleanRemote: false, 
+                                        excludes: '', 
+                                        execCommand: '''/opt/tomcat11/bin/shutdown.sh /opt/tomcat11/bin/startup.sh''', 
+                                        execTimeout: 120000, 
+                                        flatten: false, 
+                                        makeEmptyDirs: false, 
+                                        noDefaultExcludes: false, 
+                                        patternSeparator: '[, ]+', 
+                                        remoteDirectory: '', 
+                                        remoteDirectorySDF: false, 
+                                        removePrefix: 'target', 
+                                        sourceFiles: 'target/${artifact_id}.war')], 
+                                        usePromotionTimestamp: false, 
+                                        useWorkspaceInPromotion: false, 
+                                        verbose: false)])
 }
